c5b6f04ea5f6f0d4557d4cd20002189911ae6194
Malaikathecoolgirl <malaikathecoolgirl@gmail.com>
2024-09-17 09:26:03 +0200

    Enemies

---
+++ Enemies/enemy1.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var speed = 40
+var player_chase = false
+var player = null
+
+func _physics_process(delta):
+	if player_chase:
+		position += (player.position - position)/speed
+
+	if(player.position.x - position.x) < 0:
+		$AnimatedSprite2D.play("Right")
+	else:
+		$AnimatedSprite2D.play("Left")
+
+
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body):
+	player = body
+	player_chase = false
