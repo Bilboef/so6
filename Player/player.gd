5bbcb0818fed54fb52a4c931171f4556325462f8
Warz0 <162936783+Warz0@users.noreply.github.com>
2024-09-17 14:39:16 +0200

    pas på

--- Player/player.gd
+++ Player/player.gd
@@ -4,19 +4,23 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed: int = 35
-@export var run_speed: int = 200
+@export var run_speed: int = 70
 @export var current_speed: = 35
 @export var health = 100
+@export var health_max = 100
+@export var health_min = 0
 @onready var animations = $AnimationPlayer
+
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if Input.is_action_pressed("ui_run"):
-		current_speed = run_speed
+		velocity = moveDirection * run_speed
 	else:
-		current_speed = speed
-		
 		velocity = moveDirection * current_speed
+		
+	
+		
 	
 
 
@@ -41,7 +45,7 @@ func _physics_process(delta):
 
 
 func update_health():
-	var healthbar = $Healthbar
+	var healthbar = $healthbar
 	healthbar.value = health 
 	
 	if health >= 100 :
