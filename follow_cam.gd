bb275429bd8dd4ddb741c3b8525f24b6998ed309
Christian Antonius <125864425+Bilboef@users.noreply.github.com>
2024-09-18 09:47:59 +0200

    vigtig med scener

--- follow_cam.gd
+++ follow_cam.gd
@@ -5,6 +5,8 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var player = scene_manager.player
+	follow_node = player
 	var mapRect = tilemap.get_used_rect()
 	var tileSize = tilemap.cell_quadrant_size
 	var worldSizeInPixels = mapRect.size * tileSize
